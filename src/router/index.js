b88c5f467da43ab0f23004d340cb9211cc11c158
chenny <bbc123good@163.com>
2019-08-29 08:57:48 +0800

    HH

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router' /* 引用vue路由模块，并赋值给变量Router */
-// import '/node_modules/bootstrap/dist/css/bootstrap.min.css';  /* 引入bootstrap 样式 */
-// import '/node_modules/bootstrap/dist/js/bootstrap.min.js';
 import $ from 'jquery' ;
 
 Vue.use(Router)  /* 使用路由 */
