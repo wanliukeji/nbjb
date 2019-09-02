78364812f62ee55662b054c1123ff2e844f35004
chenny <bbc123good@163.com>
2019-09-02 09:49:52 +0800

    js 正则表达

--- src/components/hello.vue
+++ src/components/hello.vue
@@ -11,7 +11,7 @@
               <ul class="hide-select" id="hide-select"></ul>
             </td>
             <td align="left" class="ta-right">
-              <span id="company_name_font"><i style="color: red;">*</i> 此行为必填项</span>
+              <span id="company_name_font" class="td-left-span"><i class="td-left-span-i">*</i> 此行为必填项</span>
             </td>
           </tr>
           <tr>
@@ -21,7 +21,7 @@
                      class="input-text"/>
             </td>
             <td align="left" class="ta-right">
-              <span id="invoice_code_font"><i style="color: red;">*</i> 此行为必填项</span>
+              <span id="invoice_code_font" class="td-left-span"><i class="td-left-span-i">*</i> 此行为必填项</span>
             </td>
           </tr>
           <tr>
@@ -88,9 +88,28 @@
 
 <script TYPE="text/javascript" CHARSET="UTF-8">
     $(function () {
-        var items = [];
-        var myMap = new Map();
-        $('#company_name').focus(function () {
+            var html = '<i style="color: red;">*</i> 此行为必填项';
+            var style = 'style="font: 12px/1.5 Tahoma, \'Microsoft Yahei\', \'Simsun\';\n' +
+                '    color: #999;\n' +
+                '    clear: both;\n' +
+                '    margin: 5px 0;\n' +
+                '    color: #999;"\n' +
+                '    left: 10px;';
+            var success = '<svg style="margin-top: 10px;" t="1567221340551" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2070" width="14" height="14"><path d="M511 69C267.4 69 69.9 266.5 69.9 510.1c0 243.6 197.5 441.1 441.1 441.1 243.6 0 441.1-197.5 441.1-441.1C952.2 266.5 754.7 69 511 69z m282 292.7L463.4 691.3l-18.6 18.6c-19.6 19.6-51.4 19.6-71 0l-18.6-18.6-126.8-126.8c-16.3-16.3-14.8-43.9 4.6-58.1 15.4-11.2 36.9-8.7 50.3 4.8l125.9 125.9 330.3-330.3c16.3-16.3 43.9-14.8 58.1 4.6 11.4 15.4 8.8 36.9-4.6 50.3z" p-id="2071" fill="#1afa29"></path></svg>';
+            var error = '<svg style=" margin-bottom: 1px;" t="1567221892863" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2845" width="14" height="14"><path d="M664.96 630.4a32 32 0 0 1-22.4 54.72 32 32 0 0 1-22.72-9.28L506.56 562.56 393.6 675.84a32 32 0 0 1-22.72 9.28 32 32 0 0 1-22.72-9.28 32 32 0 0 1 0-45.44l113.28-112.96-113.28-113.28a32 32 0 0 1 0-45.12 32 32 0 0 1 45.44 0L506.56 472l113.28-112.96a32 32 0 1 1 45.12 45.12L552 517.44zM512 64a448 448 0 1 0 448 448A448 448 0 0 0 512 64z" p-id="2846" fill="#d81e06"></path></svg>';
+
+            var flag = new Boolean(false);
+            var company_name_reg = /^[^\u0000-\u00FF]+[\.]?$/;
+            var invoice_code_reg = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{4,23}$/;
+            var regNumber = /\d+/;
+            var regString = /[a-zA-Z]+/;
+            var regNumStrAbc = /^[\\/x{4e00}\-\/x{9fa5}/A-Za-z0-9]+$/u;
+            var company_address_reg = /^[x{4e00}x{9fa5}A-Za-z0-9]+$/u;
+            var contact_name_reg = /^[x{4e00}x{9fa5}A-Za-z0-9]+$/u;
+
+            var items = [];
+            var myMap = new Map();
+            $('#company_name').focus(function () {
                 var $parent = $(this);
                 var $dom = $parent.next();
                 $dom.html('');
@@ -112,6 +131,105 @@
                 });
 
             });
+
+            $('#company_name').blur(function () {
+                var company_name = $('#company_name').val();
+                //SUCCESS
+                if (!reg_test(company_name_reg, company_name)) {
+                    $('#company_name_font').html(error + '<em ' + style + ' >公司名只能由汉字组成和中文符号组成</em>');
+                    return false;
+                } else {
+                    $('#company_name_font').html(success);
+                }
+                ;
+            });
+
+            $('#invoice_code').blur(function () {
+                var invoice_code = $('#invoice_code').val();
+                //SUCCESS
+                if (!is_Empty(invoice_code)) {
+                    if (!reg_test(invoice_code_reg, invoice_code)) {
+                        $('#invoice_code_font').html(error + '<em ' + style + ' >税号只能由字母和数字组成</em>');
+                        return false;
+                    } else {
+                        $('#invoice_code_font').html(success);
+                    }
+                    ;
+                }
+                ;
+            });
+
+            $('#bank_code').blur(function () {
+                var bank_code = $('#bank_code').val();
+                //SUCCESS
+                if (!is_Empty(bank_code)) {
+                    if (!reg_test(invoice_code_reg, bank_code)) {
+                        $('#bank_code_font').html(error + '<em ' + style + ' >开户行账号只能由字母和数字组成</em>');
+                        return false;
+                    } else {
+                        $('#bank_code_font').html(success);
+                    }
+                    ;
+                }
+            });
+
+            $('#bank').blur(function () {
+                var bank = $('#bank').val();
+                //SUCCESS
+                console.log(regNumStrAbc.test(bank) + '\n' + bank);
+                if (!is_Empty(bank)) {
+                    if (reg_test(/^[/\\x{4e00}/-\/\\x{9fa5}A-Za-z0-9]+$/u, bank)) {
+                        $('#bank_font').html(error + '<em ' + style + ' >开户行只能由汉字、字母、数字组成</em>');
+                        return false;
+                    } else {
+                        $('#bank_font').html(success);
+                    }
+                    ;
+                }
+            });
+
+            $('#company_address').blur(function () {
+                var company_address = $('#company_address').val();
+                //SUCCESS
+                if (!is_Empty(company_address)) {
+                    if (reg_test(regNumStrAbc, company_address)) {
+                        $('#company_address_font').html(error + '<em ' + style + ' >公司地址只能由汉字、字母、数字组成</em>');
+                        return false;
+                    } else {
+                        $('#company_address_font').html(success);
+                    }
+                    ;
+                }
+            });
+
+            $('#contact_name').blur(function () {
+                var contact_name = $('#contact_name').val();
+                //SUCCESS
+                if (!is_Empty(contact_name)) {
+                    if (!reg_test(/^[a-zA-Z\u4e00-\u9fa5]+$/, contact_name)) {
+                        $('#contact_name_font').html(error + '<em ' + style + ' >联系人姓名只能由汉字、字母组成</em>');
+                        return false;
+                    } else {
+                        $('#contact_name_font').html(success);
+                    }
+                    ;
+                }
+            });
+
+            $('#amount').blur(function () {
+                var amount = $('#amount').val();
+                //SUCCESS
+                var count = Number(amount);//输入金额
+                var duceapp_all_amount = 100;//充值总金额
+                if (count > duceapp_all_amount) {
+                    $('#amount_font').html(error + '<em ' + style + ' >开票金额必须小于等于充值总金额 当前充值总金额 = ' + duceapp_all_amount + ' </em>');
+                    return false;
+                } else {
+                    $('#amount_font').html(success);
+                }
+                ;
+            });
+
 
             function appendToHtml(res, $dom) {
                 items = res;
@@ -183,29 +301,17 @@
 
             //提交
             $('#cy_form').submit(function () {
-                var flag = new Boolean(false);
-                var company_name_reg = /^[^\u0000-\u00FF]+[\.]?$/;
-                var invoice_code_reg = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{4,23}$/;
-                var regNumber = /\d+/;
-                var regString = /[a-zA-Z]+/;
-                var regNumStrAbc = /^[\\/x{4e00}\-\/x{9fa5}/A-Za-z0-9]+$/u;
-                var company_address_reg = /^[x{4e00}x{9fa5}A-Za-z0-9]+$/u;
-                var contact_name_reg = /^[x{4e00}x{9fa5}A-Za-z0-9]+$/u;
+                alert(1.1);
 
-                var company_name = $('#company_name').val();
-                var invoice_code = $('#invoice_code').val();
-                var bank_code = $('#bank_code').val();
-                var bank = $('#bank').val();
-                var company_address = $('#company_address').val();
-                var amount = $('#amount').val();
-                var contact_name = $('#contact_name').val();
                 //空值判断
                 is_Null();
 
                 //正则判断
                 flag = exec(company_name_reg, invoice_code_reg, company_address_reg, contact_name_reg, company_name, invoice_code
                     , bank_code, bank, company_address, amount, contact_name, regNumber, regString, regNumStrAbc);
-                return flag;
+
+                alert(flag);
+                return false;
             });
 
             function is_Null() {
@@ -218,92 +324,6 @@
                 if (is_Empty(invoice_code)) {
                     alert('开户号码不能为空！');
                     return false;
-                }
-                ;
-            }
-
-            var html = '<i style="color: red;">*</i> 此行为必填项';
-            var style = 'style="color: red; font-size: 14px; margin-left: 0px;"';
-            var success = '<svg style="margin-top: 10px;" t="1567221340551" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2070" width="14" height="14"><path d="M511 69C267.4 69 69.9 266.5 69.9 510.1c0 243.6 197.5 441.1 441.1 441.1 243.6 0 441.1-197.5 441.1-441.1C952.2 266.5 754.7 69 511 69z m282 292.7L463.4 691.3l-18.6 18.6c-19.6 19.6-51.4 19.6-71 0l-18.6-18.6-126.8-126.8c-16.3-16.3-14.8-43.9 4.6-58.1 15.4-11.2 36.9-8.7 50.3 4.8l125.9 125.9 330.3-330.3c16.3-16.3 43.9-14.8 58.1 4.6 11.4 15.4 8.8 36.9-4.6 50.3z" p-id="2071" fill="#1afa29"></path></svg>';
-            var error = '<svg style=" margin-bottom: 1px;" t="1567221892863" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2845" width="14" height="14"><path d="M664.96 630.4a32 32 0 0 1-22.4 54.72 32 32 0 0 1-22.72-9.28L506.56 562.56 393.6 675.84a32 32 0 0 1-22.72 9.28 32 32 0 0 1-22.72-9.28 32 32 0 0 1 0-45.44l113.28-112.96-113.28-113.28a32 32 0 0 1 0-45.12 32 32 0 0 1 45.44 0L506.56 472l113.28-112.96a32 32 0 1 1 45.12 45.12L552 517.44zM512 64a448 448 0 1 0 448 448A448 448 0 0 0 512 64z" p-id="2846" fill="#d81e06"></path></svg>';
-
-            function exec(company_name_reg, invoice_code_reg, company_address_reg, contact_name_reg, company_name, invoice_code
-                , bank_code, bank, company_address, amount, contact_name, regNumber, regString, regNumStrAbc) {
-
-                //SUCCESS
-                if (!reg_test(company_name_reg, company_name)) {
-                    $('#company_name_font').html(error + '<em ' + style + ' >公司名只能由汉字组成和中文符号组成</em>');
-                    return false;
-                } else {
-                    $('#company_name_font').html(success);
-                }
-                ;
-
-                //SUCCESS
-                if (!is_Empty(invoice_code)) {
-                    if (!reg_test(invoice_code_reg, invoice_code)) {
-                        $('#invoice_code_font').html(error + '<em ' + style + ' >税号只能由字母和数字组成</em>');
-                        return false;
-                    } else {
-                        $('#invoice_code_font').html(success);
-                    }
-                    ;
-                }
-                ;
-
-                //SUCCESS
-                if (!is_Empty(bank_code)) {
-                    if (!reg_test(invoice_code_reg, bank_code)) {
-                        $('#bank_code_font').html(error + '<em ' + style + ' >开户行账号只能由字母和数字组成</em>');
-                        return false;
-                    } else {
-                        $('#bank_code_font').html(success);
-                    }
-                    ;
-                }
-
-
-                //SUCCESS
-                console.log(regNumStrAbc.test(bank) + '\n' + bank);
-                if (!is_Empty(bank)) {
-                    if (reg_test(/^[/\\x{4e00}/-\/\\x{9fa5}A-Za-z0-9]+$/u, bank)) {
-                        $('#bank_font').html(error + '<em ' + style + ' >开户行只能由汉字、字母、数字组成</em>');
-                        return false;
-                    } else {
-                        $('#bank_font').html(success);
-                    }
-                    ;
-                }
-
-                //SUCCESS
-                if (!is_Empty(contact_name)) {
-                    if (!reg_test(/^[a-zA-Z\u4e00-\u9fa5]+$/, contact_name)) {
-                        $('#contact_name_font').html(error + '<em ' + style + ' >联系人姓名只能由汉字、字母组成</em>');
-                        return false;
-                    } else {
-                        $('#contact_name_font').html(success);
-                    }
-                    ;
-                }
-
-                //SUCCESS
-                if (!is_Empty(company_address)) {
-                    if (reg_test(regNumStrAbc, company_address)) {
-                        $('#company_address_font').html(error + '<em ' + style + ' >公司地址只能由汉字、字母、数字组成</em>');
-                        return false;
-                    } else {
-                        $('#company_address_font').html(success);
-                    }
-                    ;
-                }
-
-                var count = Number(amount);//输入金额
-                var duceapp_all_amount = 100;//充值总金额
-                if (count > duceapp_all_amount) {
-                    $('#amount_font').html(error + '<em ' + style + ' >开票金额必须小于等于充值总金额 当前充值总金额 = '+duceapp_all_amount +' </em>');
-                    return false;
-                } else {
-                    $('#amount_font').html(success);
                 }
                 ;
             }
@@ -323,6 +343,78 @@
                     return true;
                 }
                 return false;
+            }
+
+            function exec(company_name_reg, invoice_code_reg, company_address_reg, contact_name_reg, company_name, invoice_code
+                , bank_code, bank, company_address, amount, contact_name, regNumber, regString, regNumStrAbc) {
+
+                //SUCCESS
+                if (!is_Empty(invoice_code)) {
+                    if (!reg_test(invoice_code_reg, invoice_code)) {
+                        $jq('#invoice_code_font').html(error + '<em ' + style + ' >税号只能由字母和数字组成</em>');
+                        return false;
+                    } else {
+                        $jq('#invoice_code_font').html(success);
+                    }
+                    ;
+                }
+                ;
+
+                //SUCCESS
+                if (!is_Empty(bank_code)) {
+                    if (!reg_test(invoice_code_reg, bank_code)) {
+                        $jq('#bank_code_font').html(error + '<em ' + style + ' >开户行账号只能由字母和数字组成</em>');
+                        return false;
+                    } else {
+                        $jq('#bank_code_font').html(success);
+                    }
+                    ;
+                }
+
+
+                //SUCCESS
+                if (!is_Empty(bank)) {
+                    if (reg_test(/^[/\\x{4e00}/-\/\\x{9fa5}A-Za-z0-9]+$/u, bank)) {
+                        $jq('#bank_font').html(error + '<&lt;>em ' + style + ' >开户行只能由汉字、字母、数字组成</em>');
+                        return false;
+                    } else {
+                        $jq('#bank_font').html(success);
+                    }
+                    ;
+                }
+
+                //SUCCESS
+                if (!is_Empty(contact_name)) {
+                    if (!reg_test(/^[a-zA-Z\u4e00-\u9fa5]+$/, contact_name)) {
+                        $jq('#contact_name_font').html(error + '<em ' + style + ' >联系人姓名只能由汉字、字母组成</em>');
+                        return false;
+                    } else {
+                        $jq('#contact_name_font').html(success);
+                    }
+                    ;
+                }
+
+                //SUCCESS
+                if (!is_Empty(company_address)) {
+                    if (reg_test(regNumStrAbc, company_address)) {
+                        $jq('#company_address_font').html(error + '<em ' + style + ' >公司地址只能由汉字、字母、数字组成</em>');
+                        return false;
+                    } else {
+                        $jq('#company_address_font').html(success);
+                    }
+                    ;
+                }
+
+                var count = Number(amount);//输入金额
+                var duceapp_all_amount = 0;//充值总金额
+                if (count > duceapp_all_amount) {
+                    $jq('#amount_font').html(error + '<em ' + style + ' >开票金额必须小于等于充值总金额 &nbsp; 当前充值总金额 = ' + duceapp_all_amount + ' </em>');
+                    return false;
+                } else {
+                    $jq('#amount_font').html(success);
+                }
+                ;
+                alert(1.01);
             }
         }
     );
@@ -396,4 +488,21 @@
     vertical-align: middle;
     padding-left: 10px;
   }
+
+  .td-left-span {
+    font: 12px/1.5 Tahoma, 'Microsoft Yahei', 'Simsun';
+    color: #999;
+    clear: both;
+    margin: 5px 0;
+    color: #999;
+  }
+
+  .td-left-span-i {
+    color: red;
+    /*margin: 0 auto;*/
+    /*top: 3px;*/
+    /*line-height: 10px;*/
+    vertical-align: middle;
+  }
+
 </style>
