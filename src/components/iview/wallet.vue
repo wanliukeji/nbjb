7b67571d5a447081459fb1e5447c4048ced23b4c
chenny <bbc123good@163.com>
2019-09-03 14:23:06 +0800

    js 正则表达

--- src/components/iview/wallet.vue
+++ src/components/iview/wallet.vue
@@ -17,35 +17,6 @@
         提现
       </button>
     </div>
-    <hr>
-
-    <div class="model-div-row">
-      <div class="model-div-row-center">
-        <p><span class="model-div-row-center-left-span">手机</span>
-          <span class="model-div-row-center-right-span">
-          <svg t="1567476046283" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
-               p-id="1728" width="17" height="10">
-            <path
-              d="M719.969 493.984L298.307 72.323c-9.417-9.418-23.192-10.912-30.767-3.337-7.575 7.574-6.08 21.349 3.337 30.767l411.91 411.908-412.412 412.408c-9.418 9.418-10.912 23.194-3.337 30.769 7.574 7.575 21.35 6.08 30.767-3.337l421.663-421.663a29.523 29.523 0 0 0 4.589-5.937c6.68-7.718 4.994-20.835-4.088-29.917z"
-              fill="#8a8a8a" p-id="1729"></path>
-          </svg>
-        </span></p>
-      </div>
-    </div>
-    <br>
-    <div class="model-div-row" style="margin-top: -40px;">
-      <div class="model-div-row-center">
-        <p><span class="model-div-row-center-left-span">微信</span>
-          <span class="model-div-row-center-right-span">
-          <svg t="1567476046283" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
-               p-id="1728" width="17" height="10">
-            <path
-              d="M719.969 493.984L298.307 72.323c-9.417-9.418-23.192-10.912-30.767-3.337-7.575 7.574-6.08 21.349 3.337 30.767l411.91 411.908-412.412 412.408c-9.418 9.418-10.912 23.194-3.337 30.769 7.574 7.575 21.35 6.08 30.767-3.337l421.663-421.663a29.523 29.523 0 0 0 4.589-5.937c6.68-7.718 4.994-20.835-4.088-29.917z"
-              fill="#8a8a8a" p-id="1729"></path>
-          </svg>
-        </span></p>
-      </div>
-    </div>
 
   </div>
 </template>
