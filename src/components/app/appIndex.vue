eb08199cbd2c7cd6da277ada8451305d22b3737c
chenny <bbc123good@163.com>
2019-10-22 17:23:22 +0800

    阿拉招聘 导航

--- src/components/app/appIndex.vue
+++ src/components/app/appIndex.vue
@@ -2,15 +2,22 @@
   <div class="model">
     <div class="top">
       <span class="left">
-        <svg t="1571735779534" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="1116" width="28" height="28"><path d="M682.424996 911.667585c-8.490372 0-17.041118-2.841721-24.091698-8.685823L199.142038 522.585079c-8.662287-7.185656-13.682627-17.853623-13.682627-29.112038s5.02034-21.926382 13.682627-29.111015L658.333298 83.941807c16.068977-13.325493 39.902802-11.06194 53.228295 4.995781 13.313214 16.081257 11.073196 39.901779-4.995781 53.228295L282.502607 493.473041 706.565812 844.756663c16.068977 13.326517 18.308995 37.147038 4.995781 53.228295C704.080202 907.004379 693.301717 911.667585 682.424996 911.667585z" p-id="1117"></path></svg>
+        <svg t="1571735779534" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
+             p-id="1116" width="28" height="28"><path
+          d="M682.424996 911.667585c-8.490372 0-17.041118-2.841721-24.091698-8.685823L199.142038 522.585079c-8.662287-7.185656-13.682627-17.853623-13.682627-29.112038s5.02034-21.926382 13.682627-29.111015L658.333298 83.941807c16.068977-13.325493 39.902802-11.06194 53.228295 4.995781 13.313214 16.081257 11.073196 39.901779-4.995781 53.228295L282.502607 493.473041 706.565812 844.756663c16.068977 13.326517 18.308995 37.147038 4.995781 53.228295C704.080202 907.004379 693.301717 911.667585 682.424996 911.667585z"
+          p-id="1117"></path></svg>
       </span>
-      <span style="text-align: center; color: black; margin: 0 auto; font-size: 22px; margin-bottom: 5px; margin-top: -5px;">阿拉招聘</span>
+      <span
+        style="text-align: center; color: black; margin: 0 auto; font-size: 22px; margin-bottom: 5px; margin-top: -5px;">阿拉招聘</span>
       <span class="right">
         <svg t="1571734414773" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
              p-id="15379" width="28" height="28"><path
           d="M0 106.5h1024M0 85.2h1024v42.7H0zM0 917.5h1024M0 896.2h1024v42.7H0z" p-id="15380"></path><path
           d="M0 512h1024M0 490.7h1024v42.7H0z" p-id="15381"></path></svg>
       </span>
+    </div>
+    <div class="model-body">
+
     </div>
     <div class="footer">
       <a class="footer-div">
@@ -94,6 +101,11 @@
     padding: 15px 10px 0px 10px;
   }
 
+  .model-body {
+    height: auto;
+    position: relative;
+    /*padding: 10px 0px 10px 0px;*/
+  }
 
   .footer {
     position: fixed;
